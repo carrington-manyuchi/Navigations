af747b0c67ec5e0bc3843dc648569601ec357b38
Carrington <Manyuchi>
2023-08-06 11:49:06 +0200

    Initial Commit

---
+++ Navigations/RootViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  Navigations
+//
+//  Created by DA MAC M1 157 on 2023/08/06.
+//
+
+import UIKit
+
+class RootViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+}
+
---
+++ Navigations/SecondVC.swift
@@ -0,0 +1,16 @@
+//
+//  SecondVC.swift
+//  Navigations
+//
+//  Created by DA MAC M1 157 on 2023/08/06.
+//
+
+import UIKit
+
+class SecondVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
